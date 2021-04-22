178f7da97fb212db1e59bfb80bf113b9b2d6ab42
bookmana <82885927+bookmana@users.noreply.github.com>
2021-04-22 10:14:06 +0900

    Update test.py

--- schedule/test.py
+++ schedule/test.py
@@ -1,3 +1,4 @@
+import datetime
 import os
 
 print(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
@@ -10,8 +11,7 @@ print(os.path.abspath(__file__))
 
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 TARGET_DIR = "/static_site_repo/_posts"
-FULL_PATH = os.path.join(BASE_DIR,TARGET_DIR)
-FULL_PATH2 = "{}{}".format(BASE_DIR, TARGET_DIR)
+FULL_PATH = "{}{}".format(BASE_DIR, TARGET_DIR)
 
 print("############################## BASE_DIR")
 print("BASE_DIR : ",BASE_DIR)
@@ -22,14 +22,17 @@ print("FULL_PATH : ",FULL_PATH2)
 
 print("############################## FULL_PATH")
 print(os.path.isdir(FULL_PATH))
-for wa in os.walk(FULL_PATH):
+for wa in os.walk(FULL_PATH)
 	file = wa[2]
 	file_cnt = len(wa[2])
 	print(file," - ",str(file_cnt))
 
-print("##############################FULL_PATH2")
-print(os.path.isdir(FULL_PATH2))
-for wa in os.walk(FULL_PATH2):
-	file = wa[2]
-	file_cnt = len(wa[2])
-	print(file," - ",str(file_cnt))
+
+content="hellow python"
+
+now = datetime.datetime.now()	
+
+file_path = """%s/%s-%s.md""" %(FULL_PATH,now.strftime('%Y-%m-%d'),'1234567')
+f = open(file_path ,'w', encoding='utf8')
+#f = open("./_posts/%s-%s.md"%(now.strftime('%Y-%m-%d'),isbn_no) ,'w', encoding='utf8')
+f.write(content)
