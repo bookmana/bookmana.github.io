f5b34fd120a458cb39528e380c3bd34dc8d1b829
bookmana <82885927+bookmana@users.noreply.github.com>
2021-04-22 10:04:18 +0900

    Update test.py

--- schedule/test.py
+++ schedule/test.py
@@ -1,6 +1,22 @@
 import os
 
 print(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+
+
 print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 print(os.path.dirname(os.path.abspath(__file__)))
 print(os.path.abspath(__file__))
+
+
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+TARGET_DIR = "/static_site_repo/_posts"
+FULL_PATH = os.path.join(BASE_DIR,TARGET_DIR)
+print("##############################")
+print("BASE_DIR : ",BASE_DIR)
+print("TARGET_DIR : ",TARGET_DIR)
+print("FULL_PATH : ",FULL_PATH)
+
+
+print("##############################")
+print(os.path.isdir(FULL_PATH))
+print(os.walk(FULL_PATH))
