92adbd0332e10c2bbca3ec67d1282866d5aa7f87
bookmana <82885927+bookmana@users.noreply.github.com>
2021-04-22 09:55:13 +0900

    Create test.py

---
+++ schedule/test.py
@@ -0,0 +1,6 @@
+import os
+
+print(os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__)))))
+print(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
+print(os.path.dirname(os.path.abspath(__file__)))
+print(os.path.abspath(__file__))
