53ead8684e45961d6a6af74f898f5e1e5441ffb9
bookmana <82885927+bookmana@users.noreply.github.com>
2023-10-24 13:38:42 +0900

    Update main.py

--- py/main.py
+++ py/main.py
@@ -193,7 +193,7 @@ if __name__ == '__main__':
 				except Exception as e:
 					print("review list ",e)
 				
-				if cnt == 1 or cnt == 4:
+				if cnt == 1:
 					try:
 						tis = tistory.AutoTistory(app_id, access_token, blog_nm)
 						tis.sendTistory(bfo, review_list)
@@ -204,7 +204,7 @@ if __name__ == '__main__':
 			else:
 				bookManaOrderInsert(isbn)		
 			
-			if cnt > 5:
+			if cnt >= 2:
 				quit()
 
 			time.sleep(1)
